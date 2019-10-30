c94e265dc183c106049677047f4844aab5767ae8
ErwinGonzalez <erwinggonzalez@galileo.edu>
2019-10-29 19:40:37 -0600

    added correct server

--- servidor.js
+++ servidor.js
@@ -1,8 +1,9 @@
-var express = require('express');
-const cors = require('cors');
-var bodyparser = require('body-parser');
-var app = express();
-app.use(bodyparser.json());
+const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+const app = express()
+app.use(bodyparser.json())
+app.use(cors())
 
 
 /*var mysql      = require('mysql');
@@ -13,29 +14,30 @@ var connection = mysql.createConnection({
   database : 'temp'
   
 });*/
-
+var now = new Date();
 app.get('/datos', function (req, res) {
-  let now= new Date();
+  
   var temp = req.query.Temperatura;
   var hum = req.query.Humedad;
   console.log("temperatura: " + temp + " humedad: " + hum +"   fecha/hora:"+now);
   //meter a la base de datos conforme van llegando
-  //connection.connect();
+  connection.connect();
 
-  // connection.query('INSERT INTO temperatura'+temp+','+hum+freq+fecham+'INTO', function(err, rows, fields) {
-  //   if (err) throw err;
-  //   console.log('The solution is: ', rows[0].solution);
-  // });
+   connection.query('INSERT INTO temperatura'+temp+','+hum+freq+fecham+'INTO', function(err, rows, fields) {
+     if (err) throw err;
+     console.log('The solution is: ', rows[0].solution);
+   });
   
-  //connection.end();
+  connection.end();
 });
 //esto va quemado por que no se va a hacer mesh
-app.post('/info', cors(),function(req,res) {
+app.post('/info',function(req,res) {
+  
   
   res.send({
     "id": 'proyecto02',
-    "url": "127.0.0.1",
-    "date": "hoy",
+    "url": "localhost:3000",
+    "date": now.toISOString(),
     "hardware": {
       "id01": {
         "tag": "Termistor",
@@ -52,11 +54,50 @@ app.post('/info', cors(),function(req,res) {
 });
 
 app.post('/search',function (req,res) {
- console.log( req.length );
-  
+ 
+ console.log(req.body);
+    res.send ({
+      "​id​": "proyecto02",
+      "​url​": "localhost:3000",
+      "​date​": now.toISOString(),
+      "​search​": {
+        "​id_hardware​": "id01",
+        "​type​": "input"
+      },
+      "​data​": {
+        "1989-12-20T07:35:49.757Z": {
+          "​sensor​": 233,
+          "​status​": false,
+          "​freq​": 30000,
+          "​text​": "Estable"
+        },
+        "1989-12-20T07:40:58.757Z": {
+          "​sensor​": 570,
+          "​status​": false,
+          "​freq​": 30000,
+          "​text​": "Estable"
+        },
+        "1989-12-20T07:45:49.757Z": {
+          "​sensor​": 770,
+          "​status​": true,
+          "​freq​": 10000,
+          "​text​": "Peligro"
+        }
+      }
+    });
 });
 
-
+app.post('/change',function (req,res) {
+  console.log(req.body);
+  res.send(
+    {
+      "​id​": "proyecto02",
+      "​url​": "localhost:3000",
+      "​date​": now.toISOString(),
+      "​status​": "ERROR"
+    }
+  );
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
